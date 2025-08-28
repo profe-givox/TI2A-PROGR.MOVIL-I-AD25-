461e38ae2a6c24f6ea9603961988c008950ff549
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-08-28 10:58:58 -0600

    Agregamod TextFiled manejamos estado y recordamos el estado

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -16,6 +16,7 @@
 package com.example.tiptime
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,6 +24,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -30,7 +32,10 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -38,6 +43,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +65,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    //val amountInput = "0"
+    var amountInput  by remember { mutableStateOf("0")}
+
+    TextField(
+        value = amountInput,
+        //onValueChange = { el -> Log.d("PERROX", el) },
+        //onValueChange = {  Log.d("PERROX", it) },
+        onValueChange = {  amountInput = it },
+        modifier = modifier
+    )
+}
+
+@Composable
 fun TipTimeLayout() {
     Column(
         modifier = Modifier
@@ -70,6 +93,11 @@ fun TipTimeLayout() {
             modifier = Modifier
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
+        )
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
         )
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
