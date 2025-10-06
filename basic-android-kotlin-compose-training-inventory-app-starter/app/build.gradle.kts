370d6d1c7cad6e23082b3decb51576bc0d6f3242
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-10-06 11:00:15 -0600

    Se agregaron las dependencias Room Database

--- basic-android-kotlin-compose-training-inventory-app-starter/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/build.gradle.kts
@@ -65,6 +65,11 @@ android {
 }
 
 dependencies {
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
     // Import the Compose BOM
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
