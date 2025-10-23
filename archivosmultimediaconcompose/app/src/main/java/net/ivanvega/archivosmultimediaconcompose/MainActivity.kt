14540ea336730cd1cb782eea9dbb96c5ef80ae5b
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-10-23 10:55:47 -0600

    Se creo un FileProvider para compartir uris de aechivos donse se alamacnarian la multimedia

--- archivosmultimediaconcompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/MainActivity.kt
+++ archivosmultimediaconcompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.ivanvega.archivosmultimediaconcompose.ui.theme.ArchivosmultimediaconcomposeTheme
 
@@ -20,14 +23,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArchivosmultimediaconcomposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    TakePictureComposable()
+                    /*Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    )*/
                 }
             }
         }
     }
+}
+
+@Composable
+fun TakePictureComposable ( modifier: Modifier = Modifier){
+
+    Column {
+
+        Button(onClick = {}) {
+            Text(stringResource(R.string.take_picture_button))
+        }
+
+    }
+
 }
 
 @Composable
--- archivosmultimediaconcompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/providers/MiFileProviderMultimedia.kt
+++ archivosmultimediaconcompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/providers/MiFileProviderMultimedia.kt
@@ -1,7 +1,29 @@
 package net.ivanvega.archivosmultimediaconcompose.providers
 
+import android.content.Context
+import android.net.Uri
+import androidx.compose.ui.geometry.RoundRect
 import androidx.core.content.FileProvider
+import net.ivanvega.archivosmultimediaconcompose.R
+import java.io.File
 
-class MiFileProviderMultimedia: FileProvider()  {
+class MiFileProviderMultimedia: FileProvider(
+    R.xml.file_paths
+)  {
+    companion object{
+        fun getImageUri (ctx : Context): Uri {
+
+            val dirIma = File(ctx.cacheDir, "images")
+            dirIma.mkdirs()
+
+            val fileImage = File.createTempFile("img_",
+                ".jpg", dirIma)
+
+            val auth = ctx.packageName + ".fileprovidermultimedia"
+
+            return getUriForFile(ctx, auth, fileImage)
+
+        }
+    }
 
 }
