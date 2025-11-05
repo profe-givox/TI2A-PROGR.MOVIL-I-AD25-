551e163e1a94efeb5be857d3a037b528bb922c6d
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-11-05 11:15:25 -0600

    Ya toma fotos y las muestra

--- archivosmultimediaconcompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/MainActivity.kt
+++ archivosmultimediaconcompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/MainActivity.kt
@@ -1,19 +1,29 @@
 package net.ivanvega.archivosmultimediaconcompose
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil3.compose.AsyncImage
+import net.ivanvega.archivosmultimediaconcompose.providers.MiFileProviderMultimedia
 import net.ivanvega.archivosmultimediaconcompose.ui.theme.ArchivosmultimediaconcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArchivosmultimediaconcomposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TakePictureComposable(modifier = Modifier.padding(innerPadding))
+                    TakePictureComposable(modifier = Modifier.padding(innerPadding).fillMaxSize())
                     /*Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -37,11 +47,52 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TakePictureComposable ( modifier: Modifier = Modifier){
 
-    Column {
+    var uriFile : Uri? = null
+    val hayFoto = remember { mutableStateOf(false) }
+    var uriPhoto = remember { mutableStateOf<Uri?>(null) }
 
-        Button(onClick = {}) {
+
+    val context = LocalContext.current
+
+
+
+    val takePictureLauncher =   rememberLauncherForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) {
+        if (it) {
+            // Handle result
+            Log.d("TAGFOTO", "TakePictureComposable: $uriFile")
+            hayFoto.value = it
+            uriPhoto.value = uriFile
+
+        }
+
+    }
+
+
+    Column (modifier = modifier) {
+
+
+        Button(onClick = {
+
+            uriFile = MiFileProviderMultimedia.getImageUri(context)
+
+            takePictureLauncher.launch(uriFile)
+        }) {
             Text(stringResource(R.string.take_picture_button))
         }
+
+
+        if (uriPhoto.value != null){
+            Log.d("URIFOTO", uriPhoto.value.toString())
+        AsyncImage(
+            model = uriPhoto.value,
+            contentDescription = stringResource(R.string.take_picture_description),
+            modifier = Modifier.fillMaxWidth()
+        )}
+
+
+
 
     }
 
